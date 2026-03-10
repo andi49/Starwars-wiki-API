301b42198ea2bc532e82e400accede850998c170
Isak Jönsson <isak.j1819@gmail.com>
2026-03-10 16:37:53 +0100

    Update characters.js
    i have Combined Akabab api and star wars databank api. to get characters affiliations from Akabab api and characters description from star wars databank api in to one array and  filtered two other arrays with sith and jedis only

--- javascript/characters.js
+++ javascript/characters.js
@@ -1,5 +1,50 @@
+//fetch star wars data from both akabab and star wars data bank api
+async function fetchStarWarsData() {
+    const akababUrl = "https://raw.githubusercontent.com/akabab/starwars-api/master/api/all.json";
+    const dataBankUrl = "https://starwars-databank-server.vercel.app/api/v1/characters?page=2&limit=total"
 
-  fetch("https://starwars-databank-server.vercel.app/api/v1/characters?page=2&limit=total")
-  .then((res) => res.json())
-  .then((json) => console.log(json))
-  .catch((error) => console.error(error))
+
+    try {
+
+        //  fetch the data paralell with eachother for faster loading
+        const [akababRes, dataBankRes] = await Promise.all([
+            fetch(akababUrl),
+            fetch(dataBankUrl)
+
+        ])
+
+        const akababData = await akababRes.json()
+        const dataBankData = await dataBankRes.json()
+        const dataBankCharacters = dataBankData.data
+
+        // combine data
+        const combineData = akababData.map(char => {
+            // find matching Names from both apis 
+            const matchNames = dataBankCharacters.find(dbChar => dbChar.name === char.name)
+            const newData = {
+                name: char.name,
+                image: char.image,
+                affiliations: char.affiliations,
+                description: matchNames ? matchNames.description : "No description found"
+            }
+
+            return newData
+        })
+        console.log('Combined data:', combineData)
+        // find sith
+        const sith = combineData.filter(char => char.affiliations?.includes("Sith"));
+        console.log("Hittade sith:", sith)
+
+        // find Jedi
+        const jedi = combineData.filter(char => char.affiliations?.includes("Jedi Order"));
+        console.log("Hittade jedi:", jedi)
+
+
+    } catch (err) {
+        console.error('does not work')
+
+
+    }
+}
+
+fetchStarWarsData()
